33a5c05bc9af2904ad1101a12ebadfc5076e992a
German Candelaria <gmco0528@gmail.com>
2016-10-24 21:44:16 -0500

    v 0.25

--- src/main/java/WebsiteFiller.java
+++ src/main/java/WebsiteFiller.java
@@ -68,30 +68,4 @@ public class WebsiteFiller implements MainInterface{
       System.out.println("Done w/ automation");
     }
     
-    public void populateValues(String strFirstName, String strMiddleName) {
-      String strTestValue;
-      WebDriver driver  = new FirefoxDriver();
-      //driver.get("https://www.usaa.com/inet/ent_membereligibility/CpModularPersonalInfo?action=executeTask&target=ModularPersonalInfoTask&operationName=INIT&wa_ref=wcu_main_banner");
-      driver.get("http://www.pvamu.edu/ece/students/current/prerequisite-override-request/");
-      //WebElement elementMiddleName = driver.findElement(By.id("MiddleName"));
-      WebDriverWait wait = new WebDriverWait(driver, 10);
-      //WebElement elementFirstName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("field_first_name")));
-      
-      //WebElement elementFirstName = driver.findElement(By.id("input_15_71"));
-      //WebElement elementMiddleName = driver.findElement(By.xpath("//*[@id=\"field_first_name\"]"));
-      //Select(driver.findElement(By.id("input_15_71"))).selectByVisibleText("Prerequisite Override");
-      Select dropdown = new Select(driver.findElement(By.id("input_15_71")));
-      dropdown.selectByVisibleText("Prerequisite Override");
-      
-      WebElement elementFirstName = driver.findElement(By.id("input_15_6_3"));
-      elementFirstName.sendKeys(strFirstName);
-
-      strTestValue = elementFirstName.getAttribute("value");
-      //driver.findElement(By.name("btnWriteReport")).click();
-      System.out.println(strTestValue);
-      driver.close();
-      System.out.println("Done");
-      System.exit(0);
-  }
-    
 }
